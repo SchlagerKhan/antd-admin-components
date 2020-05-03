8c69219a4c4a69683f8ded506b2baa8680e286cb
Kalle <kh.hansson@gmail.com>
2020-05-03 09:53:24 +0200

    Added loading to prompts

--- src/prompt/prompt/prompt.tsx
+++ src/prompt/prompt/prompt.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Modal } from 'antd';
 
 import { PromptProps } from './types';
 
 export function Prompt(props: PromptProps) {
-	const { title, content, handleOk, handleCancel } = props;
+	const { title, content, handleCancel } = props;
+	const [confirmLoading, setLoading] = useState(false);
+
+	async function handleOk() {
+		try {
+			setLoading(true);
+			await props.handleOk();
+		} catch (err) {
+			setLoading(false);
+		}
+	}
 
 	const modalProps = {
 		visible: true,
 		title,
+		confirmLoading,
 		onOk: handleOk,
 		onCancel: handleCancel,
 		children: content,
