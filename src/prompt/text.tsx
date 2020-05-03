1ded1ce100e9f4681a89dd357a249d430c634e45
Kalle <kh.hansson@gmail.com>
2020-05-03 09:53:58 +0200

    Fixed prompt label formatting

--- src/prompt/text.tsx
+++ src/prompt/text.tsx
@@ -4,10 +4,10 @@ import { BasicPromptOpts } from './prompt';
 import { formPrompt } from './form';
 
 export function textPrompt(inputs: string[], opts: BasicPromptOpts) {
-	const fields: FormFieldTemplateElement[] = inputs.map((name) => ({
-		name,
-		placeholder: name,
-	}));
+	const fields: FormFieldTemplateElement[] = inputs.map((name) => {
+		const label = name.replace(/^\w/, (c) => c.toUpperCase());
+		return { name, label };
+	});
 
 	return formPrompt(fields, opts);
 }
